f7619521adb581764dfb999e9122c2e559fb77ee
Sean McCarthy <spm64@pitt.edu>
2022-04-19 18:05:47 -0700

    fix csv import tests: set unit test data_selector since_time to the start of time
    The csv samples are dated early 2021, and the default data selection since_time is set to one year prior to runtime. All of the csv samples had 'expired'. We want to keep the default since_time behavior and not have to regularly update the dates in the csv samples.

--- tests/importer_test.py
+++ tests/importer_test.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+import pytest
 import pytz
 
 from hunter.csv_options import CsvOptions
@@ -8,17 +9,30 @@ from hunter.importer import CsvImporter, HistoStatImporter
 from hunter.test_config import CsvTestConfig, CsvMetric, HistoStatTestConfig
 
 
-def test_import_csv():
-    test = CsvTestConfig(
+SAMPLE_CSV = "tests/resources/sample.csv"
+
+
+def csv_test_config(file, csv_options=None):
+    return CsvTestConfig(
         name="test",
-        file="tests/resources/sample.csv",
-        csv_options=CsvOptions(),
+        file=file,
+        csv_options=csv_options if csv_options else CsvOptions(),
         time_column="time",
         metrics=[CsvMetric("m1", 1, 1.0, "metric1"), CsvMetric("m2", 1, 5.0, "metric2")],
         attributes=["commit"],
     )
+
+
+def data_selector():
+    selector = DataSelector()
+    selector.since_time = datetime(1970, 1, 1, 1, 1, 1, tzinfo=pytz.UTC)
+    return selector
+
+
+def test_import_csv():
+    test = csv_test_config(SAMPLE_CSV)
     importer = CsvImporter()
-    series = importer.fetch_data(test_conf=test)
+    series = importer.fetch_data(test_conf=test, selector=data_selector())
     assert len(series.data.keys()) == 2
     assert len(series.time) == 10
     assert len(series.data["m1"]) == 10
@@ -27,16 +41,9 @@ def test_import_csv():
 
 
 def test_import_csv_with_metrics_filter():
-    test = CsvTestConfig(
-        name="test",
-        file="tests/resources/sample.csv",
-        csv_options=CsvOptions(),
-        time_column="time",
-        metrics=[CsvMetric("m1", 1, 1.0, "metric1"), CsvMetric("m2", 1, 5.0, "metric2")],
-        attributes=["commit"],
-    )
+    test = csv_test_config(SAMPLE_CSV)
     importer = CsvImporter()
-    selector = DataSelector()
+    selector = data_selector()
     selector.metrics = ["m2"]
     series = importer.fetch_data(test, selector=selector)
     assert len(series.data.keys()) == 1
@@ -46,17 +53,9 @@ def test_import_csv_with_metrics_filter():
 
 
 def test_import_csv_with_time_filter():
-    test = CsvTestConfig(
-        name="test",
-        file="tests/resources/sample.csv",
-        csv_options=CsvOptions(),
-        time_column="time",
-        metrics=[CsvMetric("m1", 1, 1.0, "metric1"), CsvMetric("m2", 1, 5.0, "metric2")],
-        attributes=["commit"],
-    )
-
+    test = csv_test_config(SAMPLE_CSV)
     importer = CsvImporter()
-    selector = DataSelector()
+    selector = data_selector()
     tz = pytz.timezone("Etc/GMT+1")
     selector.since_time = datetime(2021, 1, 5, 0, 0, 0, tzinfo=tz)
     selector.until_time = datetime(2021, 1, 7, 0, 0, 0, tzinfo=tz)
@@ -68,17 +67,9 @@ def test_import_csv_with_time_filter():
 
 
 def test_import_csv_with_unix_timestamps():
-    test = CsvTestConfig(
-        name="test",
-        file="tests/resources/sample.csv",
-        csv_options=CsvOptions(),
-        time_column="time",
-        metrics=[CsvMetric("m1", 1, 1.0, "metric1"), CsvMetric("m2", 1, 5.0, "metric2")],
-        attributes=["commit"],
-    )
-
+    test = csv_test_config(SAMPLE_CSV)
     importer = CsvImporter()
-    series = importer.fetch_data(test_conf=test)
+    series = importer.fetch_data(test_conf=test, selector=data_selector())
     assert len(series.data.keys()) == 2
     assert len(series.time) == 10
     assert len(series.data["m1"]) == 10
@@ -90,18 +81,9 @@ def test_import_csv_with_unix_timestamps():
 def test_import_csv_semicolon_sep():
     options = CsvOptions()
     options.delimiter = ";"
-
-    test = CsvTestConfig(
-        name="test",
-        file="tests/resources/sample-semicolons.csv",
-        csv_options=options,
-        time_column="time",
-        metrics=[CsvMetric("m1", 1, 1.0, "metric1"), CsvMetric("m2", 1, 5.0, "metric2")],
-        attributes=["commit"],
-    )
-
+    test = csv_test_config("tests/resources/sample-semicolons.csv", options)
     importer = CsvImporter()
-    series = importer.fetch_data(test_conf=test)
+    series = importer.fetch_data(test_conf=test, selector=data_selector())
     assert len(series.data.keys()) == 2
     assert len(series.time) == 10
     assert len(series.data["m1"]) == 10
@@ -110,16 +92,9 @@ def test_import_csv_semicolon_sep():
 
 
 def test_import_csv_last_n_points():
-    test = CsvTestConfig(
-        name="test",
-        file="tests/resources/sample.csv",
-        csv_options=CsvOptions(),
-        time_column="time",
-        metrics=[CsvMetric("m1", 1, 1.0, "metric1"), CsvMetric("m2", 1, 5.0, "metric2")],
-        attributes=["commit"],
-    )
+    test = csv_test_config(SAMPLE_CSV)
     importer = CsvImporter()
-    selector = DataSelector()
+    selector = data_selector()
     selector.last_n_points = 5
     series = importer.fetch_data(test, selector=selector)
     assert len(series.time) == 5
